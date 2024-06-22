e4a666d3984dff2adaaff6f6c5c1c74e159414c8
kkumaki12 <k.kumaki12@gmail.com>
2024-06-22 18:46:31 +0900

    add logging

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"go_todo/utils"
 	"log"
 
 	"gopkg.in/ini.v1"
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
